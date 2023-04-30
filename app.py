44848588968682977240f94570bb779e681ea820
Michail Golubjev <xopkop@gmail.com>
2023-04-30 12:06:45 +0200

    finished map

--- app.py
+++ app.py
@@ -1,12 +1,50 @@
 import geopandas as gpd
-import pandas as pd
-import plotly.express as px
-from dash import Dash, dcc, html, Input, Output
-import dash_bootstrap_components as dbc
-import numpy as np
-import plotly.graph_objs as go
 import os
+import dash_leaflet as dl
+import numpy as np
+from dash import Dash, html
+from dash_extensions.javascript import Namespace
+import dash_bootstrap_components as dbc
+import plotly.express as px
 from itertools import cycle
+from typing import List, Dict
+from pyproj import Transformer
+
+def get_rotation_angle(feature: Dict) -> float:
+    transformer = Transformer.from_crs(4326, 25832)  # Create a transformer from EPSG:4326 to EPSG:25832
+    coordinates = feature['geometry']['coordinates']  # Extract coordinates from the feature geometry
+    pc = [transformer.transform(coord[1], coord[0]) for coord in coordinates]  # Transform coordinates to EPSG:25832
+
+    # Calculate the side lengths of the rectangle
+    side_lengths = [np.linalg.norm(np.array(pc[i]) - np.array(pc[i - 1])) for i in range(1, 5)]
+
+    # Find the indices of the two longest sides (opposite sides in a rectangle)
+    long_side_indices = sorted(range(len(side_lengths)), key=lambda i: side_lengths[i])[-2:]
+
+    p1 = np.array(pc[long_side_indices[0]])
+    p2 = np.array(pc[long_side_indices[0] + 1])
+    v = p1 - p2
+    
+    angle_rad = np.arctan2(v[0], v[1])  # Calculate the angle in radians using arctan2
+    angle_deg = np.degrees(angle_rad) + 90  # Convert the angle from radians to degrees
+
+    # The next two conditions ensure the text is never upside down by limiting the rotation angle between -90 and 90 degrees
+    if angle_deg < -90:
+        angle_deg += 180
+    elif angle_deg > 90:
+        angle_deg -= 180
+
+    return angle_deg  # Return the rotation angle in degrees
+
+def get_centroid(gdf: gpd.GeoDataFrame, feature: Dict, properties: List[str]) -> List[float]:
+    query_string = " and ".join([f"{prop} == '{feature['properties'][prop]}'" for prop in properties])
+    filtered_gdf = gdf.query(query_string)
+    
+    if not filtered_gdf.empty:
+        centroid = filtered_gdf.iloc[0].geometry.centroid
+        return [centroid.y, centroid.x]
+    else:
+        return None
 
 cwd = os.getcwd()
 
@@ -15,109 +53,87 @@ def find(name, path):
         if name in files:
             return os.path.join(root, name)
 
+colors:str = cycle(px.colors.qualitative.Plotly)
+
 # Instantiate dash app
 dash_app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-app = dash_app.server        
+app = dash_app.server
 
-# Read the GeoJSON file
+# Read the ViewFrames GeoJSON file
 viewFramesFileName = 'ViewFrames.geojson'
+# Next line is an example ViewFrames.geojson feature
+# {"type":"FeatureCollection","name":"ViewFrames","features":[{"type":"Feature","properties":{"DwgNumber":"001","PdfLink":"https://damgaardri.sharepoint.com/:b:/s/022-1226Egedal-KrogholmvejEtape1/EWqAQXO0zMxAjEQMbuu7oeYB_jAvgBvUlLHfRp8Om0dGlA"},"geometry":{"coordinates":[[699583.9,6185399],[699535.3,6185198.5],[699466.9,6185215],[699515.4,6185415.5],[699583.9,6185399]],"type":"LineString"}}]}
 gdf = gpd.read_file(find(viewFramesFileName, cwd), crs='EPSG:25832')
 gdf = gdf.to_crs(epsg=4326)
+geojson_data = gdf.__geo_interface__
+# add color property to each feature in the GeoJSON
+for feature in geojson_data["features"]:
+    feature["properties"]["color"] = next(colors)
 
-# Populate the dict with number and link
-rectangle_sharepoint_urls = dict(zip(gdf['DwgNumber'], gdf['PdfLink']))  # Update with PDF links
+# Read the FJV GeoJSON file
+#fjvFileName = 'Fjernvarme_old.geojson'
+fjvFileName = 'Fjernvarme.geojson'
+gdf_fjv = gpd.read_file(find(fjvFileName, cwd), crs='EPSG:25832')
+gdf_fjv = gdf_fjv.to_crs(epsg=4326)
+fjv_data = gdf_fjv.__geo_interface__
 
-# function to get colors
-colors = cycle(px.colors.qualitative.Plotly)
+# Split the GeoJSON data into two separate GeoJSON objects based on the feature color property
+non_black_features = [feature for feature in fjv_data["features"] if feature["properties"]["color"] != "#000000"]
+black_features = [feature for feature in fjv_data["features"] if feature["properties"]["color"] == "#000000"]
 
-def generate_traces(gdf):
-    traces = []
-    for index, row in gdf.iterrows():
-        
-        color = next(colors)
+non_black_data = {"type": "FeatureCollection", "features": non_black_features}
+black_data = {"type": "FeatureCollection", "features": black_features}
 
-        coordinates = np.array(row.geometry.coords)
-        lon, lat = coordinates[:, 0], coordinates[:, 1]
-        trace_lines = go.Scattermapbox(
-            lat=lat,
-            lon=lon,
-            mode="lines",
-            line=dict(width=2, color=color),
-            customdata=[index],
-            hoverinfo='none',
-            showlegend=True,
-            name=row['DwgNumber']
-        )
-        traces.append(trace_lines)
-        
-        # Add a trace for the viewframe number
-        number_coords = row.geometry.centroid.coords[0]
-        trace_numbers = go.Scattermapbox(
-            lat=[number_coords[1]],
-            lon=[number_coords[0]],
-            mode="text",
-            text=[row['DwgNumber']],
-            customdata=[index],
-            hovertemplate='<b>Viewframe:</b> %{customdata}<br>',
-            textposition="middle center",
-            textfont=dict(size=14, color=color),
-            showlegend=False
-        )
-        traces.append(trace_numbers)
-        
-    return traces
+# Create the namespace and the layout
+ns = Namespace("myNamespace", "mySubNamespace")
 
-map_json = gdf.__geo_interface__
-
-graph = dbc.Card([dcc.Graph(id='map')])
-dash_app.layout = dbc.Container(
-    [
-        html.H1('Interactive Plotly Dash Map'),
-        html.Hr(),
-        dbc.Row(
-            [
-                dbc.Col(graph, width=12),
-            ]
-        )
-    ], fluid=True
+dash_app.layout = html.Div([
+    dl.Map(center=[np.mean(gdf.geometry.centroid.y), np.mean(gdf.geometry.centroid.x)], 
+           zoom=17, maxZoom=25, children=[
+        dl.TileLayer( # this styles base map
+    url = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
+    attribution = '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> ',
+    maxNativeZoom=20
+        ),
+            # fjv geometry
+    dl.GeoJSON(
+    data=non_black_data, id="fjv_non_black", options=dict(style=ns("style_fjv"))
+    ),
+        # fjv geometry
+    dl.GeoJSON(
+    data=black_data, id="fjv_black", options=dict(style=ns("style_fjv"))
+    ),
+        # view frames rectangles
+        dl.GeoJSON(
+    data=geojson_data, id="viewframes", options=dict(style=ns("style"))
+    ),
+    
+    # text marker
+    *[
+        dl.DivMarker(position=[get_centroid(gdf, feature, ['DwgNumber'])[0],
+                             get_centroid(gdf, feature, ['DwgNumber'])[1]],
+                             iconOptions={'className': 'custom-div-icon'},
+                   children=
+                   html.A(
+    html.Div(
+    f"{feature['properties']['DwgNumber']}",
+    className='custom-text', # assets/custom.css
+    style={'color': f"{feature['properties']['color']}",
+           'transform': f"rotate({get_rotation_angle(feature)}deg)",
+           'font-size': '18px',  # Adjust the font size
+           'font-weight': 'bold',  # Adjust the font weight
+           }),
+           href = feature['properties']['PdfLink'], target='_blank' # This is html.A
+           ))
+          for feature in geojson_data["features"]]
+    ], style={'width': '100%', 'height': '100vh', 'margin': "auto", "display": "block"}, id="map")
+]
 )
 
-@dash_app.callback(
-    Output('map', 'figure'),
-    Input('map', 'clickData'))
-def display_map(clickData):
-    fig = go.Figure(data=generate_traces(gdf))
-    
-    fig.update_layout(
-        margin={"r": 0, "t": 0, "l": 0, "b": 0},
-        mapbox=go.layout.Mapbox(
-            style="carto-positron",
-            zoom=15,
-            center_lat=gdf.geometry.centroid.y.mean(),
-            center_lon=gdf.geometry.centroid.x.mean(),
-        )
-    )
-    
-    if clickData:
-        clicked_id = clickData["points"][0]["customdata"]
-        pdf_url = rectangle_sharepoint_urls.get(clicked_id)
-        if pdf_url:
-            fig.add_annotation(
-                x=clickData["points"][0]["lon"],
-                y=clickData["points"][0]["lat"],
-                text=f'<a href="{pdf_url}" target="_blank">Open PDF</a>',
-                showarrow=True,
-                font=dict(size=12, color="black"),
-                bgcolor="white",
-                bordercolor="black",
-                borderwidth=1,
-                opacity=0.8
-            )
-    return fig
-
-# alternative table from dbc
-def generate_dbc_table(dataframe):
-    return dbc.Table.from_dataframe(dataframe, striped=True, bordered=True, hover=True, responsive=True)
+# @dash_app.callback(Output("click-output", "children"), Input("viewframes", "click_feature"))
+# def on_viewframe_click(feature):
+#     if feature is not None:
+#         return f"You clicked on viewframe {feature['properties']['id']}"
 
 if __name__ == '__main__':
     dash_app.run_server(debug=True)
--- assets/custom_functions.js
+++ assets/custom_functions.js
@@ -7,6 +7,23 @@ window.myNamespace = Object.assign({}, window.myNamespace, {
                 fillOpacity: 0.5,
                 weight: 2
             };
+        },
+        style_fjv: function(feature){
+            if (feature.geometry.type === 'Polygon') {
+                return {
+                    color: feature.properties.color,
+                    fillColor: feature.properties.color,
+                    fillOpacity: 1,
+                    weight: 1,
+                };
+            } else if (feature.geometry.type === 'LineString') {
+                return {
+                    color: feature.properties.color,
+                    fillColor: feature.properties.color,
+                    fillOpacity: 1,
+                    weight: 1
+                };
+            }
         }
     }
 });
